b36326378b63e2a2903314f5d1009154d89605e8
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-11-22 22:54:42 +0700

    start app shop book Gateway

---
+++ server-shopBook/src/main/java/iuh/fit/servershopBook/ServerShopBookApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.servershopBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerShopBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerShopBookApplication.class, args);
+	}
+
+}
