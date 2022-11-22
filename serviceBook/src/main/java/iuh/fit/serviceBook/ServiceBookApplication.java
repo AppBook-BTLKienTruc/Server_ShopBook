95dbaa5c522fe4d03e667c882ed237fa073fa386
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-11-22 22:23:19 +0700

    start BT - start service Book

---
+++ serviceBook/src/main/java/iuh/fit/serviceBook/ServiceBookApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.serviceBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBookApplication.class, args);
+	}
+
+}
---
+++ serviceBook/src/test/java/iuh/fit/serviceBook/ServiceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.serviceBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
