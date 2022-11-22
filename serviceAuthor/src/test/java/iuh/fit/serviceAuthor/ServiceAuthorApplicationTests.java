711e312a6f2b704c40c723cec015005b5ad1156b
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-11-22 22:37:23 +0700

    start service Author

---
+++ serviceAuthor/src/test/java/iuh/fit/serviceAuthor/ServiceAuthorApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.serviceAuthor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
